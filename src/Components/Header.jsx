8af55b9e7420483dcea1724a524c1286fe616039
Taiwo Adetunji <dontiwo5020@yahoo.com>
2025-03-29 05:39:53 +0100

    created header component

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <Link to=""><img src="/images/titantrust.png" alt="logo" /></Link>  
+        </div>
+    </div>
+  )
+}
+
+export default Header
